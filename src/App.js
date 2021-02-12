98e72f6f75c890c2b2a06cd38ddb6e2db8fce5ac
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-11 20:39:51 -0500

    start using theme provider

--- src/App.js
+++ src/App.js
@@ -6,7 +6,23 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import TextField from "@material-ui/core/TextField"
+import { makeStyles } from "@material-ui/core/styles"
 
+const useStyles = makeStyles({
+  root :{
+    background: 'linear-gradient(45deg, #333, #999)',
+    border: 0,
+    borderRadius: 15,
+    color: 'white',
+    padding: '0 30px'
+
+  }
+})
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Sytled Button</Button>
+}
 
 function CheckboxExample() {
   const [checked, setChecked] = useState(true);
@@ -32,6 +48,7 @@ function CheckboxExample() {
 function App() {
   return (
     <div className='App'>
+      <ButtonStyled /> <br/>
       <TextField
         variant='filled'
         color='primary'
