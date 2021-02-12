913389c9b03a76f0fd5c2e2e70163fc9c15d9e79
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-11 19:43:34 -0500

    working with checkboxes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Checkbox from '@material-ui/core/Checkbox'
 
 function App() {
   return (
     <div className='App'>
-      <Button 
+      <ButtonGroup>
+      <Button  
+        startIcon={<SaveIcon/>}
         variant='contained' 
         color='primary'>
-        Hello
+        Save
       </Button>
+      <Button  
+        startIcon={<DeleteIcon/>}
+        variant='contained' 
+        color='secondary'>
+        Discard
+      </Button>
+      </ButtonGroup>
     </div>
   );
 }
