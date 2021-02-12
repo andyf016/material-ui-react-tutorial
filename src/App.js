3aa7a7935361612f5c6a5ac38b008f48eaec1353
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-11 19:56:00 -0500

    start on text input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
+import { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
-import SaveIcon from '@material-ui/icons/Save'
-import DeleteIcon from '@material-ui/icons/Delete'
-import Checkbox from '@material-ui/core/Checkbox'
+import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+function CheckboxExample() {
+  const [checked, setChecked] = useState(true);
+  return (
+    <div>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={checked}
+            icon={<DeleteIcon/>}
+            checkedIcon={<SaveIcon/>}
+            onChange={(e) => setChecked(e.target.checked)}
+            inputProps={{
+              "aria-label": "secondary checkbox",
+            }}
+          />
+        } label="Testing Checkbox"
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className='App'>
+      <CheckboxExample />
       <ButtonGroup>
-      <Button  
-        startIcon={<SaveIcon/>}
-        variant='contained' 
-        color='primary'>
-        Save
-      </Button>
-      <Button  
-        startIcon={<DeleteIcon/>}
-        variant='contained' 
-        color='secondary'>
-        Discard
-      </Button>
+        <Button startIcon={<SaveIcon />} variant='contained' color='primary'>
+          Save
+        </Button>
+        <Button
+          startIcon={<DeleteIcon />}
+          variant='contained'
+          color='secondary'
+        >
+          Discard
+        </Button>
       </ButtonGroup>
     </div>
   );
